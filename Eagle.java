44b9a400283c1f74f3dd8f9c5523ed92d891741f
MaraLfs <99500975+MaraLfs@users.noreply.github.com>
2022-10-25 10:36:12 +0200

    ajout interface dly

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly{
 
     private boolean flying;
     private int altitude;
@@ -20,5 +20,30 @@ public class Eagle extends Bird {
     @Override
     public String sing() {
         return "Screech!";
+    }
+
+    @Override
+    public void takeOff() {
+        System.out.println("Et c'est partiiiiiii");
+    }
+
+    @Override
+    public int ascend(int meters) {
+        this.altitude = this.altitude + meters;
+            System.out.println(this.getName() + " swims upward, altitude : " + this.altitude);
+        return this.altitude;
+    }
+
+    @Override
+    public int descend(int meters) {
+        this.altitude = this.altitude - meters;
+            System.out.printf(this.getName() + " swims descend, altitude : " + this.altitude);
+        return this.altitude;
+    }
+
+    @Override
+    public void land() {
+        System.out.println("pnc aux portes, armement des toboggans");
+
     }
 }
